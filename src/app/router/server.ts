8dba064733ce56be9106603dfff845523ec7e60a
Chiko <2452559902@qq.com>
2024-08-05 23:41:19 +0800

    fixed: router module

--- src/app/router/server.ts
+++ src/app/router/server.ts
@@ -5,10 +5,6 @@ const app = new Koa();
 
 app.use(router.routes());
 
-app.use(ctx => {
-    ctx.body = "我是根路径默认展示文本"
-})
-
 export function run() {
     app.listen(3000, () => {
         console.log('server is run in ==> http://localhost:3000');
